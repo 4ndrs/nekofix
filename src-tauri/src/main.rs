d30e12c74d4c7bcfcb14d0f8b475680c0e3d0b35
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-08-17 00:03:32 +0200

    Manually set the gdk backend to x11
    This is a workaround for the decorations showing up on wayland

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    std::env::set_var("GDK_BACKEND", "x11");
+
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
